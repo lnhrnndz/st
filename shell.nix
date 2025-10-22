71dd82563706f77c964d44525500f6d693ebca88
Leon Hernandez <48255337+lnhrnndz@users.noreply.github.com>
2025-10-22 14:36:11 +0200

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,11 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.gnumake
+    pkgs.pkg-config
+    pkgs.xorg.libX11
+    pkgs.xorg.libXft
+    pkgs.xorg.libXinerama
+  ];
+}
